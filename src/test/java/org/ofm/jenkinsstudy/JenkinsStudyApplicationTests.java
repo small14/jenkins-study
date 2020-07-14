86f8c79cff2d8bb538c10295232d3d7f7633ba7b
tianlei <tianlei@3indnet.com>
2020-07-14 10:37:13 +0800

    1.jenkins-study

---
+++ src/test/java/org/ofm/jenkinsstudy/JenkinsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ofm.jenkinsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
